43a810452af34020bd5d4c22335dc3afb7be659e
smaregadee_adm <smaregadee@astek.mu>
2018-08-13 14:45:11 +0400

    SME : Added the new LoginFilterAttribute class and added it to the list of GlobalFilters (see FilterConfig.cs). Also added log4net dll and configured the web.config to enable logging. Finally added the method Application_Error() for logging unhandled exceptions.

--- Web/Global.asax.cs
+++ Web/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Utils;
 using Web.Models;
 
 namespace Web
@@ -16,10 +17,18 @@ namespace Web
         {
             // set the database initializer to null to prevent EF from creating the database
             Database.SetInitializer<ApplicationDbContext>(null);
+            log4net.Config.XmlConfigurator.Configure();
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+
+        protected void Application_Error()
+        {
+            var ex = Server.GetLastError();
+            Log.Error(ex.Message + " " + ex.StackTrace);
         }
     }
 }
---
+++ Web/Helpers/LoginFilterAttribute.cs
@@ -0,0 +1,60 @@
+﻿using log4net;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Utils;
+
+namespace Web.Helpers
+{
+    /// <summary>
+    /// Logs the ActionExecuting and ActionExecuted when the 'IsDebugEnabled' is enabled. The 'ActionExecuted' handler calculates the time taken in milliseconds for the request to complete.
+    /// </summary>
+    public class LoginFilterAttribute : ActionFilterAttribute
+    {
+        private const string StopwatchKey = "DebugLoggingStopWatch";
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (Log.IsDebugEnabled)
+            {
+                var loggingWatch = Stopwatch.StartNew();
+                filterContext.HttpContext.Items.Add(StopwatchKey, loggingWatch);
+
+                var message = new StringBuilder();
+                message.Append(string.Format("Executing controller {0}, action {1}",
+                    filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+                    filterContext.ActionDescriptor.ActionName));
+
+                Log.Debug(message.ToString());
+            }
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            if (Log.IsDebugEnabled)
+            {
+                if (filterContext.HttpContext.Items[StopwatchKey] != null)
+                {
+                    var loggingWatch = (Stopwatch)filterContext.HttpContext.Items[StopwatchKey];
+                    loggingWatch.Stop();
+
+                    long timeSpent = loggingWatch.ElapsedMilliseconds;
+
+                    var message = new StringBuilder();
+                    message.Append(string.Format("Finished executing controller {0}, action {1} - time spent {2}",
+                        filterContext.ActionDescriptor.ControllerDescriptor.ControllerName,
+                    filterContext.ActionDescriptor.ActionName,
+                    timeSpent));
+
+                    Log.Debug(message.ToString());
+                    filterContext.HttpContext.Items.Remove(StopwatchKey);
+                }
+            }
+        }
+    }
+}
